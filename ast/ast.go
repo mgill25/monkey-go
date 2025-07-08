d75479c1422a94343d2d2f21d1b3429a19fe201e
Manish Gill <gill.manish90@gmail.com>
2025-07-08 02:29:23 +0200

    basic recursive descent parser

---
+++ ast/ast.go
@@ -0,0 +1,49 @@
+package ast
+
+import "github.com/mgill25/monkey-go/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode() // dummy method to work around Go compiler's limitations...
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+// First implementation of Node:
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+// Identifiers
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+
+// Let Statement
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
